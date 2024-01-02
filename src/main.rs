8c0a9e3b89f3793567d86406a9d67dfbcfe959b2
Xiaotian Guo <guoxiaotian@gmail.com>
2024-01-01 20:24:54 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
+
+fn main() -> anyhow::Result<()> {
+    esp_idf_sys::link_patches();
+
+    let peripherals = Peripherals::take().unwrap();
+    // let mut led = PinDriver::output(peripherals.pins.gpio4)?;
+    let mut led = PinDriver::output(peripherals.pins.gpio2)?; // built-in led
+
+    println!("Hello world!");
+    loop {
+        led.set_high()?;
+        // we are sleeping here to make sure the watchdog isn't triggered
+        FreeRtos::delay_ms(1000);
+
+        led.set_low()?;
+        FreeRtos::delay_ms(1000);
+    }
+}
